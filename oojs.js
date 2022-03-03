af273cb85cb51fd3d9a8db57723a7fc73422279e
Sergelen <sergelen.eu@gmail.com>
2022-03-03 14:19:21 +0800

    copy done

--- oojs.js
+++ oojs.js
@@ -1,41 +1,43 @@
 const getAllFood = async () => {
-  return await fetch("https://dev-api.mstars.mn/api/foods", {
+  let res = await fetch("https://dev-api.mstars.mn/api/foods", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
   });
+  const data = await res.json();
+  return data;
 };
+const foodbtn = document.querySelector("#start");
 
+//Food class that has properties: category, category_id, image, discount, price, name, stock
 class Food {
   constructor(category, category_id, image, discount, price, name, stock) {
     this.category = category;
     this.category_id = category_id;
     this.image =
-      "https://mtars-fooddelivery.s3.ap-southeast-1.amazonaws.com/" + image;
+      "https://mtars-fooddelivery.s3.ap-southeast-1.amazonaws.com" + image;
     this.discount = discount;
     this.price = price;
     this.name = name;
     this.stock = stock;
   }
+
+  //and methods: addProduct(increase stock number), removeProduct(decrease stock number)
   addProduct() {
     this.stock += 1;
-    return true;
   }
   removeProduct() {
     this.stock -= 1;
-    return true;
   }
 }
+foodbtn.addEventListener("click",()=>{
 
-let foodarray = [];
+const foodd = document.querySelector(".foods");
 
-getAllFood()
-  .then((response) => response.json())
-  .then((response) => {
-    response.data.forEach((e) => {
-      foodarray.push(
-        new Food(
+getAllFood().then((e) => {
+    foodArr = e.data.map((e) => {
+      return new Food(
           e.category,
           e.category_id,
           e.image,
@@ -43,27 +45,52 @@ getAllFood()
           e.price,
           e.name,
           e.stock
-        )
-      );
+        );
     });
+  
+foodArr.map((e) => {
+  let card = document.createElement("p");
+  card.innerHTML = 
+  `<div class="card">
+  <img src="${e.image}" alt="" class="mainImg"/>
+  <div class="badge">${e.discount}%</div>
+  <h2>${e.name}</h2>
+  <div class="price">
+  <p class="activePrice"> ${new Intl.NumberFormat().format(e.price - (e.price * e.discount) / 100)}₮${" "} </p>
+  <strike class="strike-dark">${new Intl.NumberFormat().format(e.price)}₮ </strike>
+  </div>
+  <div id="${e.name}stock">Stock: ${e.stock} </div>
+  <div class="btns">
+  <button id="${e.name}+">+</button>
+  <button id="${e.name}-">-</button>
+  </div>
+
+  </div>`;
+
+  foodd.appendChild(card);
+
+  document.getElementById(`${e.name}+`).addEventListener("click",()=>{
+    console.log(e.stock);
+    e.addProduct();
+    document.getElementById(`${e.name}stock`).innerHTML = `Stock: ${e.stock}`;
   });
 
-console.log(foodarray);
+  document.getElementById(`${e.name}-`).addEventListener("click",()=>{
+    console.log(e.stock);
+    e.removeProduct();
 
-setTimeout(() => {
-  let div1 = document.createElement("div");
+    document.getElementById(`${e.name}stock`).innerHTML = `Stock: ${e.stock}`;
+  });
+});
+});
+});
+  
 
-  div1.setAttribute("class", "card");
 
-  let name1 = document.createElement("h2");
-  name1.innerHTML = foodarray[0].name;
 
-  div1.appendChild(name1);
-  document.querySelector(".foods").appendChild(div1);
-}, 1000);
 
-//Food class that has properties: category, category_id, image, discount, price, name, stock
-//and methods: addProduct(increase stock number and return true), removeProduct(decrease stock number and return true)
+
+
 
 //create an array foods
 
